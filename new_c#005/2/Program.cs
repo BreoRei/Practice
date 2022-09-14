ebab0c3bb3065f2d642c32768b3f8c3b6530c901
Artem <artemii0307@gmail.com>
2022-09-14 14:56:45 +0500

    add new_c/005/2

---
+++ new_c#005/2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+void arr()
+{
+    int[] array = new int[12];
+    for (int index = 0; index < array.Length; index++)
+    {
+        array[index] = new Random().Next(-9, 10);
+        Console.Write($"{array[index]}, ");
+    }
+    Console.WriteLine();
+    int[] arr = array;
+    for (int index = 0; index < arr.Length; index++)
+    {
+        arr[index] *= -1;
+        Console.Write($"{arr[index]}, ");
+    }
+}
+arr();
