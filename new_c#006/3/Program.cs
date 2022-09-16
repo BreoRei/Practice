24e5e5ef14f6bb0f3627717283d566a306567c4a
Artem <artemii0307@gmail.com>
2022-09-16 19:13:39 +0500

    add new_c/006/3

---
+++ new_c#006/3/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 42:Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+void PrintTextToString(String text)
+{
+    Console.Write(text);
+}
+
+void PrintTextWithLineBreak(String text)
+{
+    Console.WriteLine(text);
+}
+
+void TranslationToBinary(int[] arr, int num)
+{
+    int count = 0;
+    for (int i = 0; 0 < num; i++)
+    {
+        arr[arr.Length - 1 - i] = num % 2;
+        num /= 2;
+        count++;
+    }
+    for (int i = 0; i + (arr.Length - count) < arr.Length; i++)
+    {
+        Console.Write($"{arr[i + (arr.Length - count)]}");
+    }
+}
+Console.Clear();
+int[] array = new int[8];
+PrintTextToString("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+PrintTextToString($"Число {number} в двоичной системе: ");
+TranslationToBinary(array, number);
+
+
