b380aaa196dfb05a4ff7a60359d03b24fb910689
Artem <artemii0307@gmail.com>
2022-09-16 21:42:34 +0500

    add new_c/006/5

---
+++ new_c#006/5/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью 
+// поэлементного копирования.
+
+void ArrayNew(int[] arr, int[] arr1)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr1[i] = arr[i];
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+void PrintTextWithLineBreak(String text)
+{
+    Console.WriteLine(text);
+}
+Console.Clear();
+int[] array = { 1, 2, 3, 5, 7, 9, 2, 6 };
+int[] arrayNew = new int[array.Length];
+PrintTextWithLineBreak("Заданный массив: ");
+PrintArray(array);
+ArrayNew(array, arrayNew);
+PrintTextWithLineBreak("");
+PrintTextWithLineBreak("Копия массива: ");
+PrintArray(arrayNew);
